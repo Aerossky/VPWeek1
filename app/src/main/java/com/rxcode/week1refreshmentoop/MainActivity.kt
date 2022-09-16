e9c52316a49a29e4560b60cd43960be1e5d61bff
Aerossky <riskyradeonrx560@gmail.com>
2022-09-16 08:18:29 +0700

    first commit

---
+++ app/src/main/java/com/rxcode/week1refreshmentoop/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rxcode.week1refreshmentoop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
